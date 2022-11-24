50292d84d233d373569358a9162e3b745229b5a2
JoranEngelund <56642663+JoranEngelund@users.noreply.github.com>
2022-11-24 09:13:26 +0100

    Created handlers/register.mjs ->
    Added: Async fetch function that sends a HTTP POST request to the API server.
    
    Imported: registrationError function that checks for errors from response object

---
+++ src/js/handlers/register.mjs
@@ -0,0 +1,32 @@
+import { registrationError } from "../error-messages/reg-errors.mjs";
+
+/**
+ * // Async fetch function that sends a HTTP POST request to the API server,
+ *  with an object with the properties of a registered user
+ * @param {string} url // string of the API url
+ * @param {object} profile // Object of user properties
+ * @param {string} method // Type of method from the form
+ * @example
+ * ```js
+ * //
+ * register(url, profile, POST);
+ * // returns {id: , name: '', email: '', banner: '', avatar: ''}
+ * ```
+ */
+export async function register(url, profile, method) {
+  try {
+    const response = await fetch(url, {
+      method: method,
+      headers: {
+        "Content-Type": "application/json; charset=UTF-8",
+      },
+      body: JSON.stringify(profile),
+    });
+    console.log(response);
+    const json = await response.json();
+    registrationError(response);
+    console.log(json);
+  } catch (error) {
+    console.log(error);
+  }
+}
