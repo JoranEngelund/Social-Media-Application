ecf5364337646494170ba4fbfc5d956f3397b59c
JoranEngelund <56642663+JoranEngelund@users.noreply.github.com>
2022-11-25 10:25:57 +0100

    Added path handler in index.mjs ->
    Imported: loginUser
    Added: If statement to handle path, and run the correct function based on the path

--- src/js/index.mjs
+++ src/js/index.mjs
@@ -1,5 +1,14 @@
 // Main js file that will be connected to all .html files, all functionality will be imported in here.
 
 import { registerUser } from "./auth/registration.mjs";
+import { loginUser } from "./auth/login.mjs";
 
-registerUser();
+const path = window.location.pathname;
+console.log(path);
+
+if (path === "/index.html") {
+  loginUser();
+} else if (path === "/registration.html") {
+  registerUser();
+}
+
