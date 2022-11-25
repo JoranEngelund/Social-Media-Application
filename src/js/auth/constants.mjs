3cd6a467e052c7214766c6ace1b00dc9c1075f2c
JoranEngelund <56642663+JoranEngelund@users.noreply.github.com>
2022-11-25 10:21:57 +0100

    Updated new constants ->
    Added: login variable with path for login
    Added: login api with correct path
    Added: posts variable with path for posts
    Added: posts api url for fetching posts

--- src/js/auth/constants.mjs
+++ src/js/auth/constants.mjs
@@ -1,3 +1,7 @@
 const API_BASE_URL = "https://api.noroff.dev/api/v1";
 const register = "/social/auth/register";
 export const API_REG_URL = `${API_BASE_URL}${register}`;
+const login = "/social/auth/login";
+export const API_LOGIN_URL = `${API_BASE_URL}${login}`;
+const posts = "/social/posts";
+export const API_POSTS_URL = `${API_BASE_URL}${posts}`;
