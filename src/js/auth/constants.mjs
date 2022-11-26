53f17a9d8852d068920786fa8d0da8a0423c6072
JoranEngelund <56642663+JoranEngelund@users.noreply.github.com>
2022-11-26 12:46:09 +0100

    Added flags on posts endpoints

--- src/js/auth/constants.mjs
+++ src/js/auth/constants.mjs
@@ -3,5 +3,5 @@ const register = "/social/auth/register";
 export const API_REG_URL = `${API_BASE_URL}${register}`;
 const login = "/social/auth/login";
 export const API_LOGIN_URL = `${API_BASE_URL}${login}`;
-const posts = "/social/posts";
+const posts = "/social/posts?_author=true&_reactions=true&_comments=true";
 export const API_POSTS_URL = `${API_BASE_URL}${posts}`;
