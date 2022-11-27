009fdd063738037fa691dde9be3bbb750ea30a80
JoranEngelund <56642663+JoranEngelund@users.noreply.github.com>
2022-11-27 17:44:36 +0100

    Added enpoint for sending posts to api

--- src/js/auth/constants.mjs
+++ src/js/auth/constants.mjs
@@ -5,3 +5,5 @@ const login = "/social/auth/login";
 export const API_LOGIN_URL = `${API_BASE_URL}${login}`;
 const posts = "/social/posts?_author=true&_reactions=true&_comments=true";
 export const API_POSTS_URL = `${API_BASE_URL}${posts}`;
+const sendPost = "/social/posts";
+export const API_SEND_URL = `${API_BASE_URL}${sendPost}`;
