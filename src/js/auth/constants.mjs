2a61f0bd73036d43890432585e633ef33134cb65
JoranEngelund <56642663+JoranEngelund@users.noreply.github.com>
2022-11-29 16:58:21 +0100

    Added export on const API_BASE_URL

--- src/js/auth/constants.mjs
+++ src/js/auth/constants.mjs
@@ -1,4 +1,4 @@
-const API_BASE_URL = "https://api.noroff.dev/api/v1";
+export const API_BASE_URL = "https://api.noroff.dev/api/v1";
 const register = "/social/auth/register";
 export const API_REG_URL = `${API_BASE_URL}${register}`;
 const login = "/social/auth/login";
