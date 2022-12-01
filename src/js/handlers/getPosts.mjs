823399d1926d457ba33dd80503b72172f51f3d15
JoranEngelund <56642663+JoranEngelund@users.noreply.github.com>
2022-12-01 16:27:23 +0100

    updated getPosts.mjs
    Imported: reactionListener function
    Added: querySelectorAll on reaction elements in posts
    Added: Called function reactionListener and sent reaction elements as argument

--- src/js/handlers/getPosts.mjs
+++ src/js/handlers/getPosts.mjs
@@ -4,6 +4,7 @@ import * as check from "../error-messages/allPosts-error.mjs";
 import { getForm } from "../posts/comment.mjs";
 import { getEditForm } from "../posts/edit.mjs";
 import { getDelete } from "../posts/delete.mjs";
+import { reactionListener } from "../posts/reaction.mjs";
 
 /**
  * // Async function that sends get request with authorization token to retrieve all posts from API server
@@ -61,6 +62,8 @@ export async function getPosts(url) {
         );
       }
     );
+    const getReaction = document.querySelectorAll(".send-reaction");
+    reactionListener(getReaction);
     const deleteBtn = document.querySelectorAll(".delete-post");
     getDelete(deleteBtn);
     const editForm = document.querySelectorAll(".edit-form");
