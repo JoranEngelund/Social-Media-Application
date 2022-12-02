6c33c8ce3af91f9c1ae9fe0c9f91e1b7f65c10b3
JoranEngelund <56642663+JoranEngelund@users.noreply.github.com>
2022-12-02 12:41:02 +0100

    imported listeners function from listeners.mjs and called it in setup function
    all rights reserved - Benjamin Løndal

--- src/js/posts/setup.mjs
+++ src/js/posts/setup.mjs
@@ -1,9 +1,19 @@
 import { getPosts } from "../handlers/getPosts.mjs";
 import { renderPost } from "./render.mjs";
 import { API_POSTS_URL } from "../auth/constants.mjs";
+import * as run from "./listeners.mjs";
 
+/**
+ * // function that sets up an environment where it fetches all posts from the API endpoint, renders and displayes them on a feed and also sets up an environment for commenting, editing, reacting and sending a post on the home feed
+ * @example
+ * ```js
+ * // call the function setup()
+ * setup()
+ * ```
+ */
 export async function setup() {
   const posts = await getPosts(API_POSTS_URL);
   console.log(posts);
   renderPost(posts);
+  run.listeners();
 }
