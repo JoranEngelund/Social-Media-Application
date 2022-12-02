f8b694239f58294e3b062c9e1425290c0fc65c04
JoranEngelund <56642663+JoranEngelund@users.noreply.github.com>
2022-12-02 12:35:39 +0100

    Updated comment.mjs ->
    Changed: JSDocs description
    Changed: function name from getForm -> commentListener
    Added: querySelector to grab comment forms

--- src/js/posts/comment.mjs
+++ src/js/posts/comment.mjs
@@ -2,17 +2,15 @@ import { API_BASE_URL } from "../auth/constants.mjs";
 import { sendComment } from "../handlers/sendComment.mjs";
 
 /**
- * // function that takes a retrieved comment-form from a try block and sends it into a forEach with a addEventListener, which sends the form data to api server and posts it as a comment
- * @param {*} forms // querySelectorAll forms from the rendered posts
+ * // function that takes a retrieves comment-forms and sends it into a forEach with a addEventListener, which sends the form data to api server and posts it as a comment
  * @example
  * ```js
- * // retrieve a form by querySelector all inside a try block
- * const commentForm = document.querySelectorAll(".comment-form");
-    getForm(commentForm);
+ * // call the function in a scope where it can access .comment-form (form) elements
+    commentListener();
     ```
  */
-export function getForm(forms) {
-  const commentForm = forms;
+export function commentListener() {
+  const commentForm = document.querySelectorAll(".comment-form");
   commentForm.forEach((formElement) => {
     formElement.addEventListener("submit", (event) => {
       event.preventDefault();
