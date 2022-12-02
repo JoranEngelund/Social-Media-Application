f1401ac185a4b77e836b084c6ed440062a0828c8
Benjamin Løndal <benjamin.londal@gmail.com>
2022-12-02 19:59:41 +0100

    creater searchlistener and filter function

---
+++ src/js/posts/searchPost.mjs
@@ -0,0 +1,23 @@
+import { renderPost } from "../posts/render.mjs";
+
+export function setSearchListener(posts) {
+  const search = document.querySelector(".search-bar");
+
+  search.oninput = function (event) {
+    const searchValue = event.target.value.trim().toLowerCase();
+    const filteredPosts = filterPosts(searchValue, posts);
+    renderPost(filteredPosts);
+  };
+}
+
+function filterPosts(searchValue, posts) {
+  const postContainer = document.getElementById("post-container");
+  postContainer.innerHTML = "";
+  const filteredPosts = posts.filter(function (postData) {
+    if (postData.title.toLowerCase().includes(searchValue)) {
+      return true;
+    }
+  });
+
+  return filteredPosts;
+}
