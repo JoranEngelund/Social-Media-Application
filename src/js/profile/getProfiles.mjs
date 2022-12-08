d854cbd7424ac2d8a73f38e006522d2437955a22
Benjamin Løndal <benjamin.londal@gmail.com>
2022-12-08 21:44:56 +0100

    calling specific profile

--- src/js/profile/getProfiles.mjs
+++ src/js/profile/getProfiles.mjs
@@ -1,4 +1,6 @@
 import { API_PROFILE_URL } from "../auth/constants.mjs";
+import { getMyProfile } from "../handlers/getProfile.mjs";
+import { renderProfile } from "./renderProfile.mjs";
 
 /**
  * // function that sends get request with authorization token to retrieve a specific post from API server
@@ -14,8 +16,7 @@ export function displayProfile() {
   const params = new URLSearchParams(queryString);
   const name = params.get("name");
   const PROFILE_URL = `${API_PROFILE_URL}${name}?_followers=true&_following=true`;
-  getPost(PROFILE_URL);
-  console.log(PROFILE_URL);
+  getMyProfile(PROFILE_URL);
 }
 
 displayProfile();
