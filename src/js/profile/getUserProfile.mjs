965fe30e9cd226fed42a07262b80bec179d9efdd
Benjamin Løndal <benjamin.londal@gmail.com>
2022-12-11 22:05:02 +0100

    further file management fixes

--- src/js/profile/getUserProfile.mjs
+++ src/js/profile/getUserProfile.mjs
@@ -1,10 +1,20 @@
 import { renderProfile } from "./renderProfile.mjs";
+import { renderProfilePosts } from "../posts/renderProfilePosts.mjs";
 import { getMyProfile } from "../handlers/getProfile.mjs";
 import * as storage from "../storage/index.mjs";
+import { API_PROFILE_URL } from "../auth/constants.mjs";
+import { getPost } from "../handlers/getPost.mjs";
 
-export async function loadProfile() {
+export async function getUserProfile() {
   const user = storage.load("profile").name;
   const API_PROFILE_URL = `https://api.noroff.dev/api/v1/social/profiles/${user}?_followers=true&_following=true&_posts=true`;
   const profiles = await getMyProfile(API_PROFILE_URL);
   renderProfile(profiles);
+}
+
+export async function getProfilePosts() {
+  const user = storage.load("profile").name;
+  const API_PROFILEPOSTS_URL = `${API_PROFILE_URL}${user}/posts?_author=true&_reactions=true&_comments=true`;
+  const profilePosts = await getPost(API_PROFILEPOSTS_URL);
+  renderProfilePosts(profilePosts);
 }
--- src/js/profile/setup.mjs
+++ src/js/profile/setup.mjs
@@ -1,11 +1,8 @@
-import * as storage from "../storage/index.mjs";
-import { getPost } from "../handlers/getPost.mjs";
-import { renderProfilePosts } from "../posts/renderProfilePosts.mjs";
 import * as run from "../posts/listeners.mjs";
 import { profileListeners } from "./listeners.mjs";
 import { getSpecificProfile } from "./getSpecificProfile.mjs";
 import { getSpecificProfilePosts } from "./getSpecificProfile.mjs";
-import { loadProfile } from "./getUserProfile.mjs";
+import { getProfilePosts, getUserProfile } from "./getUserProfile.mjs";
 
 /**
  * //Function that sets up profile using getMyProfile() fetch and initializing renderProfile()
@@ -22,11 +19,8 @@ export function setupSpecificProfile() {
 }
 
 export async function setupProfile() {
-  const user = storage.load("profile").name;
-  const postURL = `https://api.noroff.dev/api/v1/social/profiles/${user}/posts?_author=true&_reactions=true&_comments=true`;
-  const profilePosts = await getPost(postURL);
-  renderProfilePosts(profilePosts);
-  loadProfile();
+  getUserProfile();
+  getProfilePosts();
   run.listeners();
   profileListeners();
 }
