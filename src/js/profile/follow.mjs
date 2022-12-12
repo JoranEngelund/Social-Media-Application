eead661e6ad4a2ed7a89e1cf1e74183c5be8d672
JoranEngelund <56642663+JoranEngelund@users.noreply.github.com>
2022-12-12 15:32:27 +0100

    Update follow.mjs

--- src/js/profile/follow.mjs
+++ src/js/profile/follow.mjs
@@ -5,7 +5,6 @@ import { API_PROFILE_URL } from "../auth/constants.mjs";
  * // Function that gets the name property from the queryString and passes it in the URL sent to the endpoint
  * then uses the followUser() function to send the request to follow profile
  * @example
-
  * followProfile();
  */
 export function followProfile() {
