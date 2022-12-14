f28040d3c25fc77e430eac9375d2461164401399
JoranEngelund <56642663+JoranEngelund@users.noreply.github.com>
2022-12-14 11:51:22 +0100

    Re-factoring and re-structuring filter ->
    Re-factoring and re-structuring of the filter functionality.

--- src/js/handlers/filteredTagPost.mjs
+++ src/js/handlers/filteredTagPost.mjs
@@ -1,12 +1,5 @@
 import * as storage from "../storage/index.mjs";
 import * as check from "../error-messages/allPosts-error.mjs";
-import { createPosts } from "../posts/templates/createPost.mjs";
-import { reactionListener } from "../posts/reaction.mjs";
-import { commentListener } from "../posts/comment.mjs";
-import { editListener } from "../posts/edit.mjs";
-import { deleteListener } from "../posts/delete.mjs";
-import { toggleLoadingIndicator } from "../loader/loadingIndicator.mjs";
-import { setSearchListener } from "../posts/searchPost.mjs";
 
 /**
  * // Async fetch call the sends a GET request to the server with the provided URL, returning the posts filtered by one tag name
@@ -27,47 +20,8 @@ export async function filteredTagPost(url) {
         Authorization: `Bearer ${token}`,
       },
     });
-    const posts = await response.json();
     check.responseError(response);
-    posts.forEach(
-      ({
-        title,
-        created,
-        id,
-        media,
-        updated,
-        body,
-        tags,
-        author,
-        comments,
-        _count,
-        reactions,
-      }) => {
-        const { name, avatar } = author;
-        const { comments: commentCount, reactions: reactionCount } = _count;
-        createPosts(
-          title,
-          created,
-          id,
-          media,
-          updated,
-          body,
-          tags,
-          name,
-          avatar,
-          comments,
-          commentCount,
-          reactionCount,
-          reactions
-        );
-      }
-    );
-    reactionListener();
-    commentListener();
-    editListener();
-    deleteListener();
-    toggleLoadingIndicator(posts);
-    setSearchListener(posts);
+    return await response.json();
   } catch (error) {
     check.allPostsError(error);
     console.log(error);
