508de3cee72404bb428be44f23b1c7c5ba2b2938
JoranEngelund <56642663+JoranEngelund@users.noreply.github.com>
2022-12-14 11:53:22 +0100

    Imported and called new functions name in routher

--- src/js/index.mjs
+++ src/js/index.mjs
@@ -2,8 +2,8 @@ import { registerUser } from "./auth/registration.mjs";
 import { setup } from "./posts/setup.mjs";
 import { setupProfile } from "./profile/setup.mjs";
 import { signout } from "./auth/logout.mjs";
-import { filterTags } from "./filter/filterTags.mjs";
-import { sortPosts } from "./sorter/sortType.mjs";
+import { setupFilterTags } from "./filter/setupFiltering.mjs";
+import { setupSorting } from "./sorter/setupSorting.mjs";
 import { setupSpecificPost } from "./posts/setupSpecific.mjs";
 import { setupSpecificProfile } from "./profile/setup.mjs";
 import { followProfile } from "./profile/follow.mjs";
@@ -20,10 +20,10 @@ if (path === "/" || path === "/index.html") {
   setup();
   signout();
 } else if (path === "/post-filter.html") {
-  filterTags();
+  setupFilterTags();
   signout();
 } else if (path === "/post-sorted.html") {
-  sortPosts();
+  setupSorting();
   signout();
 } else if (path === "/post-specific.html") {
   setupSpecificPost();
